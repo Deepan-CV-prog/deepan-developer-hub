d02f9da8f160e42e97e4c07df4b47cb129821d6d
Deepan-CV-prog <deepan.october@gmail.com>
2026-06-22 12:12:41 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function showPage(pageId){
+
+    const pages =
+        document.querySelectorAll(".page");
+
+    pages.forEach(page => {
+        page.style.display = "none";
+    });
+
+    document.getElementById(pageId).style.display = "block";
+}
+
+showPage("home");
